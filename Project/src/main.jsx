19c27183e20f19046f972092b433a4fe373129b1
Priyansu <priyansusaswat03@gmail.com>
2024-11-01 10:40:50 +0530

    Deployment Bugs

--- Project/src/main.jsx
+++ Project/src/main.jsx
@@ -7,7 +7,7 @@ import store from "./store/store.js";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { AuthLayout, Login } from "./components/index.js";
-import AddPost from "./pages/AddPost";
+import Addpost from "./pages/Addpost";
 import Signup from "./pages/Signup";
 import EditPost from "./pages/EditPost";
 import Post from "./pages/Post";
@@ -52,7 +52,7 @@ const router = createBrowserRouter([
         element: (
           <AuthLayout authentication>
             {" "}
-            <AddPost />
+            <Addpost />
           </AuthLayout>
         ),
       },
@@ -99,7 +99,7 @@ const router = createBrowserRouter([
         element: (
           <AuthLayout authentication>
             {" "}
-            <AddPost />
+            <Addpost />
           </AuthLayout>
         ),
       },
